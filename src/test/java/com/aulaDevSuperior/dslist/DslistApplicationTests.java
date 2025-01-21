c42694dbdf493d7b67fd938adbdbb31f60d06697
matheus azevedo <matheusazevedo435@gmail.com>
2025-01-21 01:36:43 -0300

    first-commit

---
+++ src/test/java/com/aulaDevSuperior/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaDevSuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
